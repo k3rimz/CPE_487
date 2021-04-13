6c81ee0cd1a9edd77d9d8676cc971a90df398f33
k3rimz <60765337+k3rimz@users.noreply.github.com>
2021-04-13 12:48:14 -0400

    Update acceleromter.vhd

--- acceleromter.vhd
+++ acceleromter.vhd
@@ -4,3 +4,16 @@ LIBRARY IEEE;
 USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.STD_LOGIC_ARITH.ALL;
 USE IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+
+ENTITY accelerometer IS
+    PORT (
+
+    );
+END bat_n_ball;
+
+  
+  
+ARCHITECTURE Behavioral OF  accelerometer IS
+
+END Behavioral;
